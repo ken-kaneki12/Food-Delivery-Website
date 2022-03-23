5174f5df19ed73f0d11034c42d54cc846f27c07f
goku <ashfaqulislam4566@gmail.com>
2022-03-23 20:15:28 +0600

    d

--- backend/library.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const bcrypt=require('bcryptjs');
-const jwt=require('jsonwebtoken');
-const Joi=require('@hapi/joi');
-const dotenv=require('dotenv')
-const router=require('express').Router();
-
-module.exports={express,mongoose,bcrypt,jwt,Joi,dotenv,router};
